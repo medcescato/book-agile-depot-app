f851d5d813e780e4cfa9e394ddb25073087f8b1f
medcescato <medcescato@gmail.com>
2012-04-01 18:44:52 -0300

    comandos do git

--- checkout.sh
+++ checkout.sh
@@ -1,3 +1,21 @@
-git clone git@github.com:medcescato/book-agile-depot-app.git
-cd book-agile-depot-app
-git remote add origin git@github.com:medcescato/book-agile-depot-app.git
+## checkout do zero
+# git clone git@github.com:medcescato/book-agile-depot-app.git
+# cd book-agile-depot-app
+
+# ?
+# git remote add origin git@github.com:medcescato/book-agile-depot-app.git
+
+## ver o que mudou localmente desde o ultimo checkout
+# git status
+
+## trazer ultima versao (update override changes)
+# git checkout .
+
+## adicionar novos arquivos criados ao repositorio
+# git add .
+
+## commit local
+# git commit -a -m 'nome da tag'
+
+## push pro repositorio (commit)
+# git push -u origin master
