4c9fdb32f35cddb6deb1d7646b61261c827be313
medcescato <medcescato@gmail.com>
2012-07-22 16:23:35 -0300

    redirect to login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
+  before_filter :authorize
+  
   private
+  
   def current_cart
     Cart.find(session[:cart_id])
   rescue ActiveRecord::RecordNotFound
@@ -9,4 +12,13 @@ class ApplicationController < ActionController::Base
     session[:cart_id] = cart.id
     cart
   end
+  
+  protected
+  
+  def authorize
+    unless User.find_by_id(session[:user_id])
+      redirect_to login_url, notice: "Please log in"
+    end
+  end
+  
 end
